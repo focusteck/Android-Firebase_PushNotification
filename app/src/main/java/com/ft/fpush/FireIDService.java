70bbeb9e8738252b87e3eddb6fe0fa69e5bc91c3
= <saad@focusteck.com>
2018-11-08 21:17:05 +0500

    commit

---
+++ app/src/main/java/com/ft/fpush/FireIDService.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C)FT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package com.ft.fpush;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FireIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        String tkn = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Not", "Token [" + tkn + "]");
+
+    }
+}
